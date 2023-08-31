af1388ac66dc05875cf0a25f7c7f140564d6f3ff
Dodul01 <mhdodul299@gmail.com>
2023-08-31 22:59:36 +0600

    dynamic category button added

--- script/main.js
+++ script/main.js
@@ -1,8 +1,19 @@
+// Fetch Data For Category Button
 const fetchCategory = async () =>{
   const request = await fetch('https://openapi.programming-hero.com/api/videos/categories');
   const response = await request.json();
   const allData = response.data;
   
+
+  // Dynamic Button adding
+  const categoryesBtnContainer = document.getElementById('category-btn-container');
+  
+  allData.forEach((item)=>{
+    const btn = document.createElement('button');
+    btn.classList.add('btn', 'capitalize', 'm-3');
+    btn.innerText = item.category;
+    categoryesBtnContainer.appendChild(btn)
+  })
 }
 
 fetchCategory()
